42cda05799f1cc402c67c831adeebb0969869a3a
iamwave007 <iamwave007@hotmail.com>
2015-02-01 13:07:29 -0500

    copy cat

---
+++ server.js
@@ -0,0 +1,70 @@
+// server.js
+
+    // set up ========================
+    var express  = require('express');
+    var app      = express();                               // create our app w/ express
+    var mongoose = require('mongoose');                     // mongoose for mongodb
+    var morgan = require('morgan');             // log requests to the console (express4)
+    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+    // configuration =================
+
+    //mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+    mongoose.connect('mongodb://localhost:27017/angular');
+    
+    var Todo = mongoose.model("Todo",{text:String});
+
+    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+    app.use(morgan('dev'));                                         // log every request to the console
+    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+    app.use(bodyParser.json());                                     // parse application/json
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(methodOverride());
+
+
+
+    app.get('/api/todos',function(req,res){
+        Todo.find(function(err, todos){
+            if(err){res.send(err)}
+                else{res.json(todos);}
+        })
+    })
+
+    app.post('/api/todos',function(req,res){
+        Todo.create({
+            text:req.body.text,
+            done:false
+        },function(err,todo){
+            if(err){res.send(err)};
+
+            Todo.find(function(err,todos){
+                if(err){res.send(err)}
+                    res.json(todos);
+            })
+        })
+    })
+
+    app.delete('/api/todos/:todo_id',function(req,res){
+        Todo.remove({
+            _id:req.params.todo_id
+        },function(err,todo){
+            if(err){res.send(err)}
+                Todo.find(function(err,todos){
+                    if(err){res.send(err)}
+                        res.json(todos);
+                })
+        })
+    })
+
+
+    app.get("*",function(req,res){
+        res.sendfile('./public/index.html')
+    })
+
+
+
+    // listen (start app with node server.js) ======================================
+    app.listen(8090);
+    console.log("App listening on port 8090");
+
